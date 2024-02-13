6fe6036413d62f86e7abed09abd764dbb05ab45b
0fficialAsim <adeola.asim@gmail.com>
2024-02-13 18:45:00 -0500

    Added Enums

--- src/fitnessdb/Instructor.java
+++ src/fitnessdb/Instructor.java
@@ -1,4 +1,9 @@
 package fitnessdb;
 
 public enum Instructor {
+    JENNIFER,
+    KIM,
+    DENISE,
+    DAVIS,
+    EMMA;
 }
--- src/fitnessdb/MemberList.java
+++ src/fitnessdb/MemberList.java
@@ -3,7 +3,16 @@ package fitnessdb;
 public class MemberList {
     private Member [] members; //holds Basic, Family, or Premium objects
     private int size; //number of objects in the array
-    private int find(Member member) { }
+
+    public static final int NOT_FOUND = -1;
+    private int find(Member member) {
+        for(int i = 0; i < members.length; i++){
+            if(members[i].equals(member)){
+                return i;
+            }
+        }
+        return NOT_FOUND;
+    }
     private void grow() { }
     public boolean contains(Member member) { }
     public boolean add(Member member) { }
