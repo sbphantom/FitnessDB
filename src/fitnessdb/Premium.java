5037bb234452c17850b91fb1078bcb5871ba6679
sbphantom <62124940+sbphantom@users.noreply.github.com>
2024-02-23 02:58:25 -0500

    overrode toString

--- src/fitnessdb/Premium.java
+++ src/fitnessdb/Premium.java
@@ -4,13 +4,13 @@ package fitnessdb;
  * @author Adeola
  */
 public class Premium extends Member {
-    private  int guestPass;
+    private int guestPass;
     private final double PREMIUMPRICE = 59.99; 
 
     public Premium(Profile profile, Date expire, Location homeStudio) {
         super(profile, expire, homeStudio);
         //TODO Auto-generated constructor stub
-        if(expire.isExpired()){ this.guestPass = Integer.parseInt(null);}
+        if(expire.isExpired()){ this.guestPass = 0;}
         else {this.guestPass = 0;}
 
     }
@@ -28,4 +28,13 @@ public class Premium extends Member {
     public void setGuestPass(int guestPass){
         this.guestPass = guestPass; 
     }
+    @Override
+    public String toString() {
+        if (isExpired()) {
+            return String.format("%s, Membership expired %s, Home Studio: %s, (Premium) guest-pass remaining: not eligible", getProfile(), getExpire(), getHomeStudio());
+        } else {
+            return String.format("%s, Membership expires %s, Home Studio: %s, (Premium) guest-pass remaining: %d", getProfile(), getExpire(), getHomeStudio(), guestPass);
+        }
+    }
+
 }
