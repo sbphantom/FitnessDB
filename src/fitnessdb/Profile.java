04b0b9b96eed80d5f43d789a8ca674254110f5a9
sbphantom <dannyonuorah@gmail.com>
2024-02-23 15:57:45 -0500

    added getters and isMinor

--- src/fitnessdb/Profile.java
+++ src/fitnessdb/Profile.java
@@ -8,10 +8,44 @@ public class Profile implements Comparable<Profile>{
     private final String lname;
     private final Date dob;
 
+    public boolean isMinor() {
+        return isMinor;
+    }
+
+    private boolean isMinor;
+
+    public String getFname() {
+        return fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
     public Profile(String fname, String lname, Date dob){
         this.fname =fname; 
         this.lname = lname; 
-        this.dob = dob; 
+        this.dob = dob;
+
+        Date dateOfMajor = dob.addYears(18);
+        System.out.println(dob + " : " + dateOfMajor);
+
+        System.out.println(dob + " : " + dateOfMajor);
+
+        if (dateOfMajor.getYear()-Date.todayDate().getYear() <= 0 || dateOfMajor.getMonth()-Date.todayDate().getMonth() <= 0){
+            this.isMinor = false;
+        }
+        else{
+            System.out.println(dob + " : " + dateOfMajor);
+            System.out.println(dob + " : " + dateOfMajor);
+            System.out.println(dateOfMajor.getYear()-Date.todayDate().getYear());
+            System.out.println(dateOfMajor.getMonth()-Date.todayDate().getMonth());
+            this.isMinor = true;
+        }
     }
 
 
@@ -28,7 +62,7 @@ public class Profile implements Comparable<Profile>{
     @Override
     public boolean equals(Object obj) {
     if(obj instanceof Profile profile){
-        return this.fname.equals(profile.fname) && this.lname.equals(profile.lname) && this.dob.equals(profile.dob) ;
+        return this.fname.equalsIgnoreCase(profile.fname) && this.lname.equalsIgnoreCase(profile.lname);
     }
     return false;
     }
@@ -36,6 +70,10 @@ public class Profile implements Comparable<Profile>{
     @Override
     public String toString() {
         return String.format("%s:%s:%s", this.fname, this.lname,this.dob);
+    }
+
+    public static void main(String[] args){
+
     }
 
 }
