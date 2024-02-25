32383e9f343805f3c6b54bdb802eb390e21d28d0
sbphantom <62124940+sbphantom@users.noreply.github.com>
2024-02-25 04:35:43 -0500

    added boolean canGuest

--- src/fitnessdb/Premium.java
+++ src/fitnessdb/Premium.java
@@ -1,17 +1,20 @@
 package fitnessdb;
+
 /**
- *
  * @author Adeola
  */
 public class Premium extends Member {
     private int guestPass;
-    private final double PREMIUMPRICE = 59.99; 
+    private final double PREMIUMPRICE = 59.99;
 
     public Premium(Profile profile, Date expire, Location homeStudio) {
         super(profile, expire, homeStudio);
         //TODO Auto-generated constructor stub
-        if(expire.isExpired()){ this.guestPass = 0;}
-        else {this.guestPass = 0;}
+        if (expire.isExpired()) {
+            this.guestPass = 0;
+        } else {
+            this.guestPass = 3;
+        }
 
     }
 
@@ -20,14 +23,19 @@ public class Premium extends Member {
         return PREMIUMPRICE;
     }
 
-
-    public int getGuessPass(){
-        return guestPass; 
+    @Override
+    public boolean canGuest() {
+        return (guestPass > 0);
     }
 
-    public void setGuestPass(int guestPass){
-        this.guestPass = guestPass; 
+    public int getGuessPass() {
+        return guestPass;
     }
+
+    public void setGuestPass(int guestPass) {
+        this.guestPass = guestPass;
+    }
+
     @Override
     public String toString() {
         if (isExpired()) {
